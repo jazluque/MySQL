b423bce917bf23157e2b277938d34d2ee192a723
jazluque <81364412+jazluque@users.noreply.github.com>
2021-06-23 12:17:55 -0300

    Add files via upload

---
+++ MySQL base de datos examen.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE examen;
+USE examen;
+CREATE TABLE estudiantes (idEstudiante varchar (11) not null primary key, nombre varchar (12) not null, apellido varchar (12) not null, nota int not null, curso varchar (40) not null);
+SHOW TABLES;
