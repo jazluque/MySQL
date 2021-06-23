f454cd7ed1f076f09aee9cdc60be08e0e852a1b5
jazluque <81364412+jazluque@users.noreply.github.com>
2021-06-23 11:35:38 -0300

    Add files via upload

---
+++ MySQL base de datos productos.sql
@@ -0,0 +1,4 @@
+CREATE TABLE productos;
+USE productos;
+CREATE TABLE info (idProducto varchar (11) primary key, nombre varchar (50), precio decimal (10,2), stock varchar (12) not null);
+SHOW TABLES;
